9d2869ff1f221d8423c9d51a065124fb92063ec6
Chirran <swainchittaranjan345@gmail.com>
2023-09-13 13:09:10 +0530

    bottom navigation bar widget is separated

--- lib/views/ui/main_screen.dart
+++ lib/views/ui/main_screen.dart
@@ -1,5 +1,6 @@
 import "package:e_commerce_app/controllers/main_screen_notifier.dart";
 import "package:e_commerce_app/views/shared/app_style.dart";
+import "package:e_commerce_app/views/shared/bottom_nav_bar.dart";
 import "package:e_commerce_app/views/shared/bottom_nav_widget.dart";
 import "package:e_commerce_app/views/ui/cart_page.dart";
 import "package:e_commerce_app/views/ui/home_page.dart";
@@ -27,64 +28,14 @@ class MainScreen extends StatelessWidget {
         return Scaffold(
           backgroundColor: const Color(0xFFE2E2E2),
           body: pageList[mainScreenNotifier.pageIndex],
-          bottomNavigationBar: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Container(
-                padding: const EdgeInsets.all(12),
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(16)),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    BottomNavWidget(
-                        onTap: () {
-                          mainScreenNotifier.pageIndex = 0;
-                        },
-                        icon: mainScreenNotifier.pageIndex == 0
-                            ? Ionicons.home
-                            : Ionicons.home_outline),
-                    BottomNavWidget(
-                        onTap: () {
-                          mainScreenNotifier.pageIndex = 1;
-                        },
-                        icon: mainScreenNotifier.pageIndex == 1
-                            ? Ionicons.search
-                            : Ionicons.search_outline),
-                    BottomNavWidget(
-                        onTap: () {
-                          mainScreenNotifier.pageIndex = 2;
-                        },
-                        icon: mainScreenNotifier.pageIndex == 2
-                            ? Ionicons.add
-                            : Ionicons.add_outline),
-                    BottomNavWidget(
-                        onTap: () {
-                          mainScreenNotifier.pageIndex = 3;
-                        },
-                        icon: mainScreenNotifier.pageIndex == 3
-                            ? Ionicons.cart
-                            : Ionicons.cart_outline),
-                    BottomNavWidget(
-                        onTap: () {
-                          mainScreenNotifier.pageIndex = 4;
-                        },
-                        icon: mainScreenNotifier.pageIndex == 4
-                            ? Ionicons.person
-                            : Ionicons.person_outline),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          bottomNavigationBar: const BottomNavBar(),
         );
       },
     );
   }
 }
+
+
 
 
 
