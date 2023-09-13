b4bf843315c2eeb5f609d062065446f4fbee2f22
Chirran <swainchittaranjan345@gmail.com>
2023-09-13 23:03:35 +0530

    tab bar and view added

--- lib/views/ui/home_page.dart
+++ lib/views/ui/home_page.dart
@@ -1,5 +1,7 @@
+import "package:cached_network_image/cached_network_image.dart";
 import "package:e_commerce_app/views/shared/app_style.dart";
 import "package:flutter/material.dart";
+import "package:flutter_vector_icons/flutter_vector_icons.dart";
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,7 +10,10 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController _tabController =
+      TabController(length: 3, vsync: this);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +22,163 @@ class _HomePageState extends State<HomePage> {
         child: Stack(
           children: [
             Container(
-              padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
+              padding: const EdgeInsets.fromLTRB(10, 25, 0, 0),
               height: MediaQuery.of(context).size.height * 0.4,
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage("assets/images/top_image.png"),
                   fit: BoxFit.fill,
+                ),
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(left: 2, bottom: 15),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Athletics Shoes",
+                      style: appStyleWithHt(
+                          32, Colors.white, FontWeight.bold, 1.5),
+                    ),
+                    Text(
+                      "Collection",
+                      style: appStyleWithHt(
+                          32, Colors.white, FontWeight.bold, 1.5),
+                    ),
+                    TabBar(
+                      indicatorSize: TabBarIndicatorSize.label,
+                      indicatorColor: Colors.transparent,
+                      controller: _tabController,
+                      isScrollable: true,
+                      labelColor: Colors.white,
+                      labelStyle: appStyleWithHt(
+                          16, Colors.white, FontWeight.bold, 0.1),
+                      unselectedLabelColor: Colors.grey.withOpacity(0.3),
+                      labelPadding: const EdgeInsets.only(right: 12, top: 0),
+                      padding: const EdgeInsets.only(left: 1),
+                      tabAlignment: TabAlignment.start,
+                      dividerColor: Colors.transparent,
+                      tabs: const [
+                        Tab(text: "Men Shoes"),
+                        Tab(text: "Women Shoes"),
+                        Tab(text: "Kid Shoes"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.25),
+              child: Container(
+                padding: const EdgeInsets.only(left: 12),
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.405,
+                          child: ListView.builder(
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  color: Colors.grey,
+                                  height: MediaQuery.of(context).size.height,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.6,
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 12, 0, 10),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Latest Shoes",
+                                    style: appStyle(
+                                        24, Colors.black, FontWeight.bold),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Show All",
+                                        style: appStyle(
+                                            22, Colors.black, FontWeight.w500),
+                                      ),
+                                      const Icon(AntDesign.caretright, size: 20)
+                                    ],
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.13,
+                          child: ListView.builder(
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(16),
+                                    ),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        // color: Colors.black38,
+                                        color: Colors.black,
+                                        blurRadius: 0.8,
+                                        offset: Offset(0, 1),
+                                      ),
+                                    ],
+                                  ),
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.12,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.28,
+                                  child: CachedNetworkImage(
+                                      imageUrl:
+                                          "https://d326fntlu7tb1e.cloudfront.net/uploads/710d020f-2da8-4e9e-8cff-0c8f24581488-GV6674.webp"),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.405,
+                          color: Colors.green,
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.405,
+                          color: Colors.amber,
+                        )
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
