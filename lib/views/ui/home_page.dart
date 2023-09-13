a04b89a588bb1df49c6e07539526c64ba5cc55b2
Chirran <swainchittaranjan345@gmail.com>
2023-09-13 15:38:56 +0530

    widget separated

--- lib/views/ui/home_page.dart
+++ lib/views/ui/home_page.dart
@@ -12,10 +12,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "This is Home",
-          style: appStyle(40, Colors.black, FontWeight.bold),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/top_image.png"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
