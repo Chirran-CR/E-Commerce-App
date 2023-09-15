9359bd734c0db068fc681af91e40c4bcec2b645c
Chirran <swainchittaranjan345@gmail.com>
2023-09-15 17:24:23 +0530

    services for sneaker is done by using json data

--- lib/models/sneaker_model.dart
+++ lib/models/sneaker_model.dart
@@ -1,78 +1,77 @@
-// To parse this JSON data, do
-//
-//     final sneakers = sneakersFromJson(jsonString);
 
-// import 'package:meta/meta.dart';
 import 'dart:convert';
 
-Sneakers sneakersFromJson(String str) => Sneakers.fromJson(json.decode(str));
+// Sneakers sneakersFromJson(String str) => Sneakers.fromJson(json.decode(str));
+List<Sneakers> sneakersFromJson(String str) => List<Sneakers>.from(json.decode(str).map((x)=> Sneakers.fromJson(x)));
 
-String sneakersToJson(Sneakers data) => json.encode(data.toJson());
+// String sneakersToJson(Sneakers data) => json.encode(data.toJson());
 
 class Sneakers {
-    final String id;
-    final String name;
-    final String category;
-    final List<String> imageUrl;
-    final String oldPrice;
-    final List<Size> sizes;
-    final String price;
-    final String description;
-    final String title;
+  final String id;
+  final String name;
+  final String category;
+  final List<String> imageUrl;
+  final String oldPrice;
+  // final List<Size> sizes;
+  final List<dynamic> sizes;
+  final String price;
+  final String description;
+  final String title;
 
-    Sneakers({
-        required this.id,
-        required this.name,
-        required this.category,
-        required this.imageUrl,
-        required this.oldPrice,
-        required this.sizes,
-        required this.price,
-        required this.description,
-        required this.title,
-    });
+  Sneakers({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.imageUrl,
+    required this.oldPrice,
+    required this.sizes,
+    required this.price,
+    required this.description,
+    required this.title,
+  });
 
-    factory Sneakers.fromJson(Map<String, dynamic> json) => Sneakers(
+  factory Sneakers.fromJson(Map<String, dynamic> json) => Sneakers(
         id: json["id"],
         name: json["name"],
         category: json["category"],
         imageUrl: List<String>.from(json["imageUrl"].map((x) => x)),
         oldPrice: json["oldPrice"],
-        sizes: List<Size>.from(json["sizes"].map((x) => Size.fromJson(x))),
+        // sizes: List<Size>.from(json["sizes"].map((x) => Size.fromJson(x))),
+        sizes: List<dynamic>.from(json["sizes"].map((x) => x)),
         price: json["price"],
         description: json["description"],
         title: json["title"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "category": category,
-        "imageUrl": List<dynamic>.from(imageUrl.map((x) => x)),
-        "oldPrice": oldPrice,
-        "sizes": List<dynamic>.from(sizes.map((x) => x.toJson())),
-        "price": price,
-        "description": description,
-        "title": title,
-    };
+  // Map<String, dynamic> toJson() => {
+  //     "id": id,
+  //     "name": name,
+  //     "category": category,
+  //     "imageUrl": List<dynamic>.from(imageUrl.map((x) => x)),
+  //     "oldPrice": oldPrice,
+  //     "sizes": List<dynamic>.from(sizes.map((x) => x.toJson())),
+  //     "price": price,
+  //     "description": description,
+  //     "title": title,
+  // };
 }
 
-class Size {
-    final String size;
-    final bool isSelected;
+// class Size {
+//     final String size;
+//     final bool isSelected;
 
-    Size({
-        required this.size,
-        required this.isSelected,
-    });
+//     Size({
+//         required this.size,
+//         required this.isSelected,
+//     });
 
-    factory Size.fromJson(Map<String, dynamic> json) => Size(
-        size: json["size"],
-        isSelected: json["isSelected"],
-    );
+//     factory Size.fromJson(Map<String, dynamic> json) => Size(
+//         size: json["size"],
+//         isSelected: json["isSelected"],
+//     );
 
-    Map<String, dynamic> toJson() => {
-        "size": size,
-        "isSelected": isSelected,
-    };
-}
+//     Map<String, dynamic> toJson() => {
+//         "size": size,
+//         "isSelected": isSelected,
+//     };
+// }
---
+++ lib/services/helper.dart
@@ -0,0 +1,55 @@
+import "package:flutter/services.dart" as the_bundle;
+import 'package:e_commerce_app/models/sneaker_model.dart';
+
+class Helper {
+  //all Male Sneakers
+  Future<List<Sneakers>> getMaleSneakers() async {
+    final data =
+        await the_bundle.rootBundle.loadString("assets/json/men_shoes.json");
+    final maleList = sneakersFromJson(data);
+    return maleList;
+  }
+
+  //all female Sneakers
+  Future<List<Sneakers>> getFemaleSneakers() async {
+    final data =
+        await the_bundle.rootBundle.loadString("assets/json/women_shoes.json");
+    final femaleList = sneakersFromJson(data);
+    return femaleList;
+  }
+
+  //all kids Sneakers
+  Future<List<Sneakers>> getKidSneakers() async {
+    final data =
+        await the_bundle.rootBundle.loadString("assets/json/kids_shoes.json");
+    final kidsList = sneakersFromJson(data);
+    return kidsList;
+  }
+
+  //get single male sneakers
+  Future<Sneakers> getMaleSneakersById(String id) async{
+    final data =
+        await the_bundle.rootBundle.loadString("assets/json/men_shoes.json");
+    final maleList = sneakersFromJson(data);
+    final sneaker = maleList.firstWhere((sneaker)=> sneaker.id == id);
+    return sneaker;
+  }
+
+  //get single female sneakers
+  Future<Sneakers> getFenaleSneakersById(String id) async{
+    final data =
+        await the_bundle.rootBundle.loadString("assets/json/women_shoes.json");
+    final femaleList = sneakersFromJson(data);
+    final sneaker = femaleList.firstWhere((sneaker)=> sneaker.id == id);
+    return sneaker;
+  }
+
+  //get single kid sneakers
+  Future<Sneakers> getKidSneakersById(String id) async{
+    final data =
+        await the_bundle.rootBundle.loadString("assets/json/kids_shoes.json");
+    final kidList = sneakersFromJson(data);
+    final sneaker = kidList.firstWhere((sneaker)=> sneaker.id == id);
+    return sneaker;
+  }
+}
